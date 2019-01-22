11d6e6c729f42c484cd6e189d9bb72f0a103c159
Ubeydullah Yavuz <38444243+Hubbey@users.noreply.github.com>
2019-01-22 20:46:56 +0300

    Add files via upload

---
+++ squirrel/tree.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   tree.h
+ * Author: hubbey
+ *
+ * Created on January 6, 2019, 2:18 AM
+ */
+
+#ifndef TREE_H
+#define TREE_H
+
+
+
+void login();
+void registeR();
+int checkup(char*, char*);
+void delay(int);
+void successful_login(char*, char*);
+void _hubbey_divide();
+
+#endif /* TREE_H */
+
